382078201a46da79eca5f4f98d04be3d802eb106
Adrien Fabre <adrien.fabre@dashlane.com>
2024-12-09 19:06:22 +0100

    2024 - Day 09 - Improve

--- libs/2024/day09/solution.py
+++ libs/2024/day09/solution.py
@@ -42,10 +42,15 @@ class Solution(BaseSolution[list[int]]):
             total += i * int(fileId)
         return total
 
-    # 113063514094 too low
-    # 113063506936 too low
+    # Used some optimizations to speed up the code (some of them probably aren't useful/are plain out bad)
+    # - Avoided calling len(disk) as much as possible (looks like it iterates through the whole list)
+    # - Keep track of where is the right pointer when splitting the empty slot into moved word+smaller empty slot
+    #   and when packing contiguous empty slots
+    # - When moving a file to empty slot, modify the empty slot tuple instead of removing/adding the tuple
     @answer(2858, 6511178035564)
     def part2(self):
+        # Transform the parsed input into a list of tuples representing the length of the file and its id
+        # or the length of the empty slot and "."
         disk: list[tuple[int, str]] = []
         for i, length in enumerate(self.parsedInput):
             if length == 0:
@@ -56,55 +61,56 @@ class Solution(BaseSolution[list[int]]):
             else:
                 disk.append((length, "."))
 
-        right = len(disk) - 1
-
-        seen: set[str] = set()
+        diskLen = len(disk)
+        right = diskLen - 1
         while right > 0:
-            # print(f"right {right} ({disk[right]})")
-            right = len(disk) - 1
-            while disk[right][1] == "." or disk[right][1] in seen:
-                # print("  skip, move right")
+            # Search the next file id to move
+            while disk[right][1] == ".":
                 right -= 1
 
+            # Search an available slot starting from the begining of the disk
             left = 0
             while left < right and (
                 disk[left][1] != "." or disk[left][0] < disk[right][0]
             ):
                 left += 1
-            # print(f"    left {left} ({disk[left]}) right {right} ({disk[right]})")
 
-            seen.add(disk[right][1])
+            # No available slot for the file id, move to the next one
             if left >= right:
-                # print("No place found")
                 right -= 1
                 continue
 
+            # Remaining space in the slot after we'll have moved the file
             diff = disk[left][0] - disk[right][0]
 
-            # print(f"diff {diff}")
-
-            # print(f"move {disk[right]} from {right} to {left}")
+            # Replace the current file by empty slots
             vright = disk[right][1]
             lright = disk[right][0]
             disk[right] = (disk[right][0], ".")
 
-            disk.pop(left)
-            disk.insert(left, (lright, vright))
+            # Move the file id to the empty slot
+            disk[left] = (lright, vright)
             if diff > 0:
+                # If the slot is bigger than the file id, insert a new empty spot after the moved file
                 disk.insert(left + 1, (diff, "."))
+                diskLen += 1
+                right += 1
 
+            # The operations can create several continuous empty slots, packs them
             i = 1
-            while i < len(disk):
+            while i < diskLen:
                 chunk = disk[i]
                 if chunk[1] == "." and disk[i - 1][1] == ".":
+                    # Here we are tempted of not removing the empty slot of length 0 to avoid mutating the list
+                    # But that makes much more slots to iterate over and so would be slower
                     disk[i] = (disk[i - 1][0] + disk[i][0], ".")
                     disk.pop(i - 1)
+                    diskLen -= 1
+                    right -= 1
                 else:
                     i += 1
 
-            # print(disk)
-            # printDisk(disk)
-
+        # Moves are all done, compute the result
         total = 0
         i = 0
         for length, val in disk:
@@ -112,7 +118,7 @@ class Solution(BaseSolution[list[int]]):
                 i += length
                 continue
 
-            for j in range(1, length + 1):
+            for _ in range(1, length + 1):
                 total += i * int(val)
                 i += 1
 
