76df3602f6126cddabb94ea2b3bca68e2c8c528c
Adrien Fabre <adrien.fabre@dashlane.com>
2024-12-16 23:41:33 +0100

    2024 - Day 16 - Reimplement using complex instead of Point

--- libs/2024/day16/solution.py
+++ libs/2024/day16/solution.py
@@ -4,6 +4,85 @@ from typing import NamedTuple
 from ...base import BaseSolution, answer
 from ...utils.point import E, N, Point, S, W
 
+# doWorkWithComplex implements the same function as doWork but uses the `complex` built-in type
+# instead of my own `Point` class to represent points and directions, it makes for a nearly 40%
+# run time improvement.
+#
+# With complex:
+# time ./main.py -d16 -l
+# Result for day 16/2024 - part 1 - livemode True: 98520
+# Result for day 16/2024 - part 2 - livemode True: 609
+# ./main.py -d16 -l  13,94s user 0,07s system 99% cpu 14,020 total
+#
+# With Point:
+# time ./main.py -d16 -l
+# Result for day 16/2024 - part 1 - livemode True: 98520
+# Result for day 16/2024 - part 2 - livemode True: 609
+# ./main.py -d16 -l  23,47s user 0,07s system 99% cpu 23,545 total
+
+
+class Input(NamedTuple):
+    g: list[list[str]]
+    start: Point
+    end: Point
+
+
+CN = 0 - 1j
+CS = 0 + 1j
+CE = 1 + 0j
+CW = -1 + 0j
+
+# front, left, right, back
+neighborsDirectionsC = {
+    CN: [(CN, 1), (CW, 1001), (CE, 1001), (CS, 2001)],
+    CS: [(CS, 1), (CE, 1001), (CW, 1001), (CN, 2001)],
+    CE: [(CE, 1), (CN, 1001), (CS, 1001), (CW, 2001)],
+    CW: [(CW, 1), (CS, 1001), (CN, 1001), (CE, 2001)],
+}
+
+
+def doWorkWithComplex(parsedInput: Input):
+    (g, start, end) = parsedInput
+
+    startC = start.x + start.y * 1j
+    endC = end.x + end.y * 1j
+
+    s = [(startC, CE, 0, [startC])]
+    seen: dict[tuple[complex, complex], int] = {}
+
+    bestTiles: set[complex] = set()
+    bestScore = math.inf
+    while s:
+        (pos, direction, score, path) = s.pop(0)
+        # print(f"{pos} {direction} {score} {len(path)}")
+
+        if score > bestScore:
+            continue
+
+        prevScore = seen.get((pos, direction))
+        if prevScore is not None and prevScore < score:
+            continue
+        seen[(pos, direction)] = score
+
+        if pos == endC:
+            if score < bestScore:
+                # print(
+                #     f"  Found new best score. Previous was: {bestScore}, new is {score}"
+                # )
+                bestScore = score
+                bestTiles = set()
+            bestTiles = bestTiles.union(path)
+            continue
+
+        # Using the default value can not happen but I want to avoid checking that we do get a value
+        for newDirection, scoreInc in neighborsDirectionsC.get(direction, []):
+            n = pos + newDirection
+            if g[int(n.imag)][int(n.real)] in ".E":
+                s.append((n, newDirection, score + scoreInc, path + [n]))
+
+    return (int(bestScore), len(bestTiles))
+
+
 # front, left, right, back
 neighborsDirections = {
     N: [(N, 1), (W, 1001), (E, 1001), (S, 2001)],
@@ -11,12 +90,6 @@ neighborsDirections = {
     E: [(E, 1), (N, 1001), (S, 1001), (W, 2001)],
     W: [(W, 1), (S, 1001), (N, 1001), (E, 2001)],
 }
-
-
-class Input(NamedTuple):
-    g: list[list[str]]
-    start: Point
-    end: Point
 
 
 def doWork(parsedInput: Input):
@@ -29,6 +102,7 @@ def doWork(parsedInput: Input):
     bestScore = math.inf
     while s:
         (pos, direction, score, path) = s.pop(0)
+        # print(f"{pos} {direction} {score} {len(path)}")
 
         if score > bestScore:
             continue
@@ -66,10 +140,12 @@ class Solution(BaseSolution[Input]):
 
     @answer(7036, 98520)
     def part1(self):
-        res = doWork(self.parsedInput)
+        # res = doWork(self.parsedInput)
+        res = doWorkWithComplex(self.parsedInput)
         return res[0]
 
     @answer(45, 609)
     def part2(self):
-        res = doWork(self.parsedInput)
+        # res = doWork(self.parsedInput)
+        res = doWorkWithComplex(self.parsedInput)
         return res[1]
