920c8d9a319ea5eb8c60f2bfc085447991e2e354
Adrien Fabre <adrien.fabre@dashlane.com>
2024-12-22 17:23:00 +0100

    2024 - Day 22 - With generators

--- libs/2024/day22/solution.py
+++ libs/2024/day22/solution.py
@@ -23,66 +23,49 @@ from ...base import BaseSolution, answer
 # 3. Calculate the result of multiplying the secret number by 2048.
 #    Then, mix this result into the secret number.
 #    Finally, prune the secret number.
-# @cache
-class Monkey:
+
+
+class Solution(BaseSolution):
     _secret: int
+    _cnt: int
 
-    def __init__(self, initialSecret: int):
+    def parseInput(self):
+        return [int(v) for v in self.lines]
+
+    def gen_secret(self, initialSecret: int, sequence_size: int):
         self._secret = initialSecret
+        self._cnt = sequence_size
 
-    def resetTo(self, initialSecret: int):
-        self._secret = initialSecret
-
-    def gen_secret(self):
-        while True:
+        while self._cnt >= 0:
+            self._cnt -= 1
             s = self._secret
             s1 = ((s * 64) ^ s) % 16777216
             s2 = ((s1 // 32) ^ s1) % 16777216
             self._secret = ((s2 * 2048) ^ s2) % 16777216
             yield self._secret
 
-    def gen_sequence(self):
-        while True:
+    def gen_sequence(self, initialSecret: int, sequence_size: int):
+        self._secret = initialSecret
+        self._cnt = sequence_size
+
+        while self._cnt >= 0:
+            self._cnt -= 1
             prev_last_digit = self._secret % 10
 
-            s = self._secret
-            s1 = ((s * 64) ^ s) % 16777216
+            s1 = ((self._secret * 64) ^ self._secret) % 16777216
             s2 = ((s1 // 32) ^ s1) % 16777216
             self._secret = ((s2 * 2048) ^ s2) % 16777216
 
             last_digit = self._secret % 10
-            last_digit_diff = last_digit - prev_last_digit
-            yield (last_digit, last_digit_diff)
-
-
-def get_sequence_orders_and_gains(start: int):
-    r: dict[tuple, int] = {}
-
-    sq = Monkey(start).gen_sequence()
-    sub_sq = deque(maxlen=4)
-    sub_sq.append(next(sq))
-    sub_sq.append(next(sq))
-    sub_sq.append(next(sq))
-
-    for _ in range(4, 2000 + 1):
-        last = next(sq)
-        sub_sq.append(last)
-        changes = tuple([v[1] for v in sub_sq])
-
-        if changes not in r:
-            r[changes] = last[0]
-
-    return r
-
-
-class Solution(BaseSolution):
-    def parseInput(self):
-        return [int(v) for v in self.lines]
+            yield (last_digit, last_digit - prev_last_digit)
 
     @answer(37327623, 20506453102)
     def part1(self):
         return sum(
-            [next(islice(Monkey(s).gen_secret(), 1999, None)) for s in self.parsedInput]
+            [
+                next(islice(self.gen_secret(s, 2000), 1999, None))
+                for s in self.parsedInput
+            ]
         )
 
     @answer(24, 2423)
@@ -91,10 +74,20 @@ class Solution(BaseSolution):
         best_gain = 0
 
         for secret in self.parsedInput:
-            sub_orders_gains = get_sequence_orders_and_gains(secret)
+            sq = self.gen_sequence(secret, 2000)
+            sub_sq = deque([next(sq)[1], next(sq)[1], next(sq)[1]], maxlen=4)
 
-            for changes in sub_orders_gains:
-                orders_gains[changes] += sub_orders_gains[changes]
-                best_gain = max(orders_gains[changes], best_gain)
+            seen_changes: set[tuple] = set()
+            for digit, diff in sq:
+                sub_sq.append(diff)
+                changes = tuple(sub_sq)
+
+                if changes in seen_changes:
+                    continue
+
+                seen_changes.add(changes)
+                orders_gains[changes] += digit
+                if best_gain < orders_gains[changes]:
+                    best_gain = orders_gains[changes]
 
         return best_gain
