7b8a6184ab4739baf4c625107205c4bd5b2c4da8
Adrien Fabre <adrien.fabre@dashlane.com>
2024-12-22 17:15:11 +0100

    2024 - Day 22 - With generators

--- libs/2024/day22/solution.py
+++ libs/2024/day22/solution.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+from itertools import islice
 
 from ...base import BaseSolution, answer
 
@@ -23,42 +24,53 @@ from ...base import BaseSolution, answer
 #    Then, mix this result into the secret number.
 #    Finally, prune the secret number.
 # @cache
-def gen_next(s: int):
-    s1 = ((s * 64) ^ s) % 16777216
-    s2 = ((s1 // 32) ^ s1) % 16777216
-    return ((s2 * 2048) ^ s2) % 16777216
+class Monkey:
+    _secret: int
+
+    def __init__(self, initialSecret: int):
+        self._secret = initialSecret
+
+    def resetTo(self, initialSecret: int):
+        self._secret = initialSecret
+
+    def gen_secret(self):
+        while True:
+            s = self._secret
+            s1 = ((s * 64) ^ s) % 16777216
+            s2 = ((s1 // 32) ^ s1) % 16777216
+            self._secret = ((s2 * 2048) ^ s2) % 16777216
+            yield self._secret
+
+    def gen_sequence(self):
+        while True:
+            prev_last_digit = self._secret % 10
+
+            s = self._secret
+            s1 = ((s * 64) ^ s) % 16777216
+            s2 = ((s1 // 32) ^ s1) % 16777216
+            self._secret = ((s2 * 2048) ^ s2) % 16777216
+
+            last_digit = self._secret % 10
+            last_digit_diff = last_digit - prev_last_digit
+            yield (last_digit, last_digit_diff)
 
 
-def get_sequence(start: int, steps: int):
-    secret = start
-    last_digit = start % 10
-
-    prev_secret = secret
-    prev_last_digit = last_digit
-    last_digit_diff = None
-
-    seq: list[tuple[int, None | int]] = []
-
-    for _ in range(steps):
-        secret = gen_next(prev_secret)
-        last_digit = secret % 10
-        last_digit_diff = last_digit - prev_last_digit
-        seq.append((last_digit, last_digit_diff))
-
-        prev_secret = secret
-        prev_last_digit = last_digit
-    return seq
-
-
-def get_sequence_orders_and_gains(sq: list[tuple[int, None | int]]):
+def get_sequence_orders_and_gains(start: int):
     r: dict[tuple, int] = {}
 
-    for i in range(4, len(sq) + 1):
-        sub_sq = sq[i - 4 : i]
+    sq = Monkey(start).gen_sequence()
+    sub_sq = deque(maxlen=4)
+    sub_sq.append(next(sq))
+    sub_sq.append(next(sq))
+    sub_sq.append(next(sq))
+
+    for _ in range(4, 2000 + 1):
+        last = next(sq)
+        sub_sq.append(last)
         changes = tuple([v[1] for v in sub_sq])
 
         if changes not in r:
-            r[changes] = sq[i - 1][0]
+            r[changes] = last[0]
 
     return r
 
@@ -69,22 +81,17 @@ class Solution(BaseSolution):
 
     @answer(37327623, 20506453102)
     def part1(self):
-        total = 0
-        for s in self.parsedInput:
-            r = s
-            for _ in range(2000):
-                r = gen_next(r)
-            total += r
-
-        return total
+        return sum(
+            [next(islice(Monkey(s).gen_secret(), 1999, None)) for s in self.parsedInput]
+        )
 
     @answer(24, 2423)
     def part2(self):
         orders_gains: dict[tuple, int] = defaultdict(int)
         best_gain = 0
+
         for secret in self.parsedInput:
-            sq = get_sequence(secret, 2000)
-            sub_orders_gains = get_sequence_orders_and_gains(sq)
+            sub_orders_gains = get_sequence_orders_and_gains(secret)
 
             for changes in sub_orders_gains:
                 orders_gains[changes] += sub_orders_gains[changes]
